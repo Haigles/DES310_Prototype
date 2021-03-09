6e4377e68603a40ee69dd4fd1a16bd9898659b21
Horyzon96 <60696034+Horyzon96@users.noreply.github.com>
2021-03-09 12:04:25 +0000

    Added AH Scene
    Added AH Scene, tinkering a bit

---
+++ DES310/Assets/AH/Scripts/CardPool.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPool : MonoBehaviour
+{
+    [Space(5)]
+    [Header("Panda Match Card Pool")]
+    [Space(10)]
+    [SerializeField] public CardInfo[] pandaMatchCards;
+
+    [Space(5)]
+    [Header("Panda Choice Card Pool")]
+    [Space(15)]
+    [SerializeField] public CardInfo[] pandaChoiceCards;
+
+}
+
+[System.Serializable]
+public class CardInfo
+{
+    public string cardName;
+
+    [Header("Strings")]
+    public string cardAge;
+    public string cardParents;
+    public string cardLocation;
+    public string cardHealth;
+
+    [Header("Values")]
+    public int valueAge;
+    public string maleParent;
+    public string femaleParent;
+    public int valueLocation;
+    public int valueHealth;
+}
---
+++ DES310/Assets/AH/Scripts/MatchSetUp.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchSetUp : MonoBehaviour
+{
+    [Space(5)]
+    [Header("Card Positions")]
+    public GameObject matchPosition;
+    public GameObject choice1Position;
+    public GameObject choice2Position;
+    public GameObject choice3Position;
+
+    [Space(5)]
+    [Header("Card Prefab")]
+    [Space(10)]
+    public GameObject cardPrefab;
+
+    public CardPool cardPool;
+    public List<CardInfo> matchCards = new List<CardInfo>();
+    public List<CardInfo> choiceCards = new List<CardInfo>();
+
+    private bool hasSetUp = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < cardPool.pandaMatchCards.Length; i++)
+        {
+            matchCards.Add(cardPool.pandaMatchCards[i]);
+        }
+        Shuffle(matchCards);
+
+        for (int i = 0; i < cardPool.pandaChoiceCards.Length; i++)
+        {
+            choiceCards.Add(cardPool.pandaChoiceCards[i]);
+        }
+        Shuffle(choiceCards);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetUp();
+        }
+    }
+
+    private void SetUp()
+    {
+        if (!hasSetUp)
+        {
+            GameObject matchCard = Instantiate(cardPrefab, matchPosition.transform.position, Quaternion.identity);
+            CardDetails matchCardDetails = matchCard.GetComponent<CardDetails>();
+
+            matchCard.transform.parent = matchPosition.transform;
+            matchCard.transform.localPosition = new Vector2(0, 0);
+
+            matchCardDetails.nameText.GetComponent<Text>().text = matchCards[0].cardName;
+            matchCardDetails.ageText.GetComponent<Text>().text = matchCards[0].cardAge;
+            matchCardDetails.parentsText.GetComponent<Text>().text = matchCards[0].maleParent + " & " + matchCards[0].femaleParent;
+            matchCardDetails.locationText.GetComponent<Text>().text = matchCards[0].cardLocation;
+            matchCardDetails.healthText.GetComponent<Text>().text = matchCards[0].cardHealth;
+
+            hasSetUp = true;
+        }
+    }
+
+    private void Shuffle(List<CardInfo> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            CardInfo temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+    }
+}

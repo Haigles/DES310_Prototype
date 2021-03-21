36386f3496bbeed4d5fa3f0f16cbec9a2e827181
Horyzon96 <60696034+Horyzon96@users.noreply.github.com>
2021-03-21 02:09:10 +0000

    Plot Highlight
    hover over plots to highlight them

---
+++ DES310/Assets/AH/Scripts/SelectEnclosure.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class SelectEnclosure : MonoBehaviour
+{
+    public GameObject plotHighlight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plotHighlight.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        plotHighlight.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        plotHighlight.SetActive(false);
+    }
+}
